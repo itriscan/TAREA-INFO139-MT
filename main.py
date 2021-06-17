fd88f457904db6b3ad0abdf40c568f62782a59ac
Ingo <ingooxs@gmail.com>
2021-06-16 23:15:58 -0400

    primer draft
    Probado con inputs:
    1
    2
    1,a,2,X,D
    q
    a
    
    Salida esperada, ahora a refactorizar

---
+++ main.py
@@ -0,0 +1,49 @@
+def strToList(a):
+	out = []
+	for i in range(len(a)):
+		out.append(a[i])
+	return out
+
+
+estadoInicial = input("Ingrese numero del estado Inicial: ")
+estadoFinal = input("Ingrese numero del estado Final: ")
+estadoActual = estadoInicial
+
+transiciones = []
+q = False
+
+while q == False:
+	## Se toma una entrada, la cual consta de 5 elementos y se ordenan de la misma forma vista en clases
+	# ['Estado inicial','CaracterInicial','EstadoFinal','CaracterFinal','Desplazamiento']
+	entr = raw_input()
+	if entr != "q":
+		entr = entr.split(",")
+		transiciones.append(entr)
+	else:
+		q = True
+	print(entr)
+
+
+palabraEntrada = raw_input()
+if palabraEntrada != "":
+	print("Palabra distinta de vacio")
+	palabra = strToList(palabraEntrada)
+	print(palabra)
+	index = 0
+
+	##while estadoActual != estadoFinal:
+		##Cosas, muchas
+	
+
+	for i in transiciones:
+		if (i[0] == str(estadoActual) ) and ( i[1] == palabraEntrada[index] ):
+			print("true",i[2],i[3],i[4])
+			estadoActual = i[2]
+			palabra[index] = i[3]
+			if i[4] == "D":
+				index +=1
+			else:
+				index -=1
+
+
+	print(transiciones,palabra,estadoActual)
